ab7a3a5ebc5bb7ecf102de3afded6c4cada72a2f
Hanul Rheem <41708091+violet-bl-cl@users.noreply.github.com>
2024-08-19 23:29:50 +1200

    fix top animation controller

--- Assets/Scripts/Player/PlayerActionStatus/TopActionStatus.cs
+++ Assets/Scripts/Player/PlayerActionStatus/TopActionStatus.cs
@@ -17,32 +17,65 @@ public class TopActionStatus : MonoBehaviour
     {
         switch (PlayerAction)
         {
-            case Action.BotJumpIdle:
+            case Action.TopIdle:
+                {
+
+                    _playerAnimator.SetBool("IsWalk", false);
+                    _playerAnimator.SetBool("IsJump", false);
+                    _playerAnimator.SetBool("IsDown", false);
+                    _playerAnimator.SetBool("IsUp", false);
+                    break;
+                }
+            case Action.TopAimUp:
+                {
+                    _playerAnimator.SetBool("IsUp", true);
+                    _playerAnimator.SetBool("IsDown", false);
+                    _playerAnimator.SetBool("IsWalk", false);
+                    _playerAnimator.SetBool("IsJump", false);
+                    break;
+                }
+            case Action.TopAimDown:
+                {
+
+                    _playerAnimator.SetBool("IsDown", true);
+                    _playerAnimator.SetBool("IsWalk", false);
+                    _playerAnimator.SetBool("IsJump", false);
+                    _playerAnimator.SetBool("IsUp", false);
+                    break;
+                }
+            case Action.TopMoveRight:
+                {
+                    _playerAnimator.SetBool("IsWalk", true);
+                    _playerAnimator.SetBool("IsJump", false);
+                    _playerAnimator.SetBool("IsDown", false);
+                    _playerAnimator.SetBool("IsUp", false);
+                    break;
+                }
+            case Action.TopJumpRight:
                 {
                     _playerAnimator.SetBool("IsJump", true);
                     _playerAnimator.SetBool("IsWalk", false);
-                    _playerAnimator.SetBool("IsIdle", false);
+                    _playerAnimator.SetBool("IsDown", false);
+                    _playerAnimator.SetBool("IsUp", false);
                     break;
                 }
-            case Action.BotJumpRight:
+            case Action.TopShootRight:
                 {
-                    _playerAnimator.SetBool("IsJump", true);
-                    _playerAnimator.SetBool("IsWalk", true);
-                    _playerAnimator.SetBool("IsIdle", false);
+                    _playerAnimator.SetTrigger("ShootRight");
                     break;
                 }
-            case Action.BotMoveRight:
+            case Action.TopShootDown:
                 {
-                    _playerAnimator.SetBool("IsWalk", true);
-                    _playerAnimator.SetBool("IsIdle", false);
-                    _playerAnimator.SetBool("IsJump", false);
+                    _playerAnimator.SetTrigger("ShootDown");
                     break;
                 }
-            case Action.BotIdle:
+            case Action.TopShootUp:
                 {
-                    _playerAnimator.SetBool("IsIdle", true);
-                    _playerAnimator.SetBool("IsWalk", false);
-                    _playerAnimator.SetBool("IsJump", false);
+                    _playerAnimator.SetTrigger("ShootUp");
+                    break;
+                }
+            default:
+                {
                     break;
                 }
         }
